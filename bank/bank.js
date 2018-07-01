dd886355e6fb8325ef053ac593c92cfd7d9a93e3
gurneypa <gurneyparker@gmail.com>
2018-06-30 21:42:33 -0600

    Start on the bank project

---
+++ bank/bank.js
@@ -0,0 +1,29 @@
+/*
+Simulate a bank with the following functions
+
+Use a variable to keep track of the balance
+
+Use a variable to keep track of the account history
+
+function deposit(amount){
+  // Make sure that the amount is a positive number
+
+  // Record the deposit in the history
+}
+
+function withdrawl(amount) {
+  // Make sure that there is enough money in the account before taking it out
+
+  // Record the withdrawl in the history
+}
+
+function showHistory() {
+  // Show the history
+}
+
+
+function showMenu() {
+  // Show a menu of what the user can do
+}
+
+*/
