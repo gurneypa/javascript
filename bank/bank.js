4e2433b3938a125d17d0ebd6048cefd121dd27f2
gurneypa <gurneyparker@gmail.com>
2018-07-01 19:08:25 -0600

    added menu

--- bank/bank.js
+++ bank/bank.js
@@ -27,26 +27,70 @@ function showMenu() {
 }
 
 */
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
 let balance = 0;
+let history = [];
+
 function deposit(amount){
   if (amount > 0){
     balance = balance + amount;
+    history.push(`deposited ${amount} balance is now ${balance}`);
   } else{
     console.error('Cannot deposit negative amounts');
   }
   return balance;
 }
 
-console.log(deposit(50));
-console.log(deposit(9.57));
-
 function withdrawl(amount){
   if (amount < balance){
     balance = balance - amount;
+    history.push(`withdrawl ${amount} balance is now ${balance}`);
   } else{
     console.error('Cannot withdrawl more than the amount of money in the bank')
   }
   return balance;
 }
 
+function showHistory(){
+  history.forEach(item => {
+    console.log(item);
+  });
+}
+
+function showMenu() {
+  console.log('Choose from the following options');
+  console.log('------------');
+  console.log('1-Deposit');
+  console.log('2-Withdrawl');
+  console.log('3-ShowHistory');
+  console.log('4-Quit');
+}
+
+process.stdin.on('data', command =>{
+  console.log(typeof command);
+  console.log(`command is ${command}`);
+  command = command.trim();
+  switch(command){
+    case '1':
+      break;
+    case '2':
+      break;
+    case '3':
+      break;
+    case '4':
+      process.exit();
+      break;
+    default:
+    console.error(`unknown command ${command}`);
+  }
+
+  showMenu();
+});
+
+console.log(deposit(50));
+console.log(deposit(9.57));
 console.log(withdrawl(25));
+showHistory();
+showMenu();
