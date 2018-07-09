6b0e14643d8ffce7e14ac22f176dd049f40818dd
gurneypa <gurneyparker@gmail.com>
2018-07-08 20:34:25 -0600

    finished and working

--- bank/bank.js
+++ bank/bank.js
@@ -29,14 +29,34 @@ function showMenu() {
 */
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
+const fs = require('fs');
 
 let balance = 0;
 let history = [];
+
+function init() {
+  try {
+    const contents = JSON.parse(fs.readFileSync('bank.json', 'utf8'));
+    balance = contents.balance;
+    history = contents.history;
+  } catch(err) {
+    console.log('Could not load bank file - starting fresh');
+  }
+}
+
+function saveInfo() {
+  const data = {
+    balance,
+    history
+  };
+  fs.writeFileSync('bank.json', JSON.stringify(data));
+}
 
 function deposit(amount){
   if (amount > 0){
     balance = balance + amount;
     history.push(`deposited ${amount} balance is now ${balance}`);
+    saveInfo();
   } else{
     console.error('Cannot deposit negative amounts');
   }
@@ -47,6 +67,7 @@ function withdrawl(amount){
   if (amount < balance){
     balance = balance - amount;
     history.push(`withdrawl ${amount} balance is now ${balance}`);
+    saveInfo();
   } else{
     console.error('Cannot withdrawl more than the amount of money in the bank')
   }
@@ -54,9 +75,14 @@ function withdrawl(amount){
 }
 
 function showHistory(){
+  console.log();
+  console.log(`Transaction history`);
+  console.log(`----------------`);
+
   history.forEach(item => {
     console.log(item);
   });
+  console.log();
 }
 
 function showMenu() {
@@ -68,29 +94,50 @@ function showMenu() {
   console.log('4-Quit');
 }
 
-process.stdin.on('data', command =>{
-  console.log(typeof command);
-  console.log(`command is ${command}`);
-  command = command.trim();
-  switch(command){
-    case '1':
-      break;
-    case '2':
-      break;
-    case '3':
-      break;
-    case '4':
-      process.exit();
-      break;
-    default:
-    console.error(`unknown command ${command}`);
+let command = 0;
+process.stdin.on('data', text =>{
+  text = text.trim();
+
+  if (command > 0) {
+    if (command === 1) {
+      deposit(parseFloat(text));
+      command = 0;
+    } else if (command === 2) {.0
+      withdrawl(parseFloat(text));
+      command = 0;
+    }
+  } else {
+    switch(text){
+      case '1':
+        console.log('Enter deposit amount:');
+        command = 1;
+        break;
+      case '2':
+        console.log('Enter withdrawl amount:');
+        command = 2;
+        break;
+      case '3':
+        showHistory();
+        break;
+      case '4':
+        process.exit();
+        break;
+      default:
+      console.error(`unknown text ${text}`);
+    }
+  }
+if (command === 0) {
+  showMenu();
   }
 
-  showMenu();
 });
-
+init();
+showMenu();
+/*
+showMenu();
 console.log(deposit(50));
 console.log(deposit(9.57));
 console.log(withdrawl(25));
 showHistory();
 showMenu();
+*/
