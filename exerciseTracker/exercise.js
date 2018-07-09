49e0e46da59a5439c8265e671caa00fd648ec1b8
David Gurney <gurneyds@familysearch.org>
2018-07-08 21:28:28 -0600

    Beginnings of an exercise tracker

---
+++ exerciseTracker/exercise.js
@@ -0,0 +1,61 @@
+/*
+Requirements
+------------------
+The purpose of this file is to track different types of exercise.
+Types of exercise might include:
+  Biking
+  Swimming
+  Tennis
+Keep track of the date and duration of each exercise event
+Write the information to a file
+Be able to show all of the exercise date, type and duration
+Set goals - track progress
+
+Use Cases (Set a goal)
+----------
+1. Display of menu of options - pick set a goal
+2. Allow the user to type in a textual description of their goal
+
+Use Case (Enter actual exercise)
+----------------------------------
+1. Display menu of options - pick enter actual exercise
+2. Allow the user to select an exercise
+3. Allow the user to enter a duration
+
+User Case (See all goals)
+--------------------------
+1. Display menu of options - pick see all goals
+2. Show the user all goals
+
+User case (See exercises of a given type - like swimming)
+-------------------------------
+1. Display menu of options - pick show specific exercise times
+2. Show the user all exercise history
+*/
+
+// TODO - Data structure to hold the history of exercise information
+
+
+function showMenu() {
+  // TODO - show the menu
+}
+
+function addGoal() {
+
+}
+
+function addExercise() {
+
+}
+
+function saveInfoToFile() {
+
+}
+
+function readInfoFromFile() {
+
+}
+
+function showSummaryOfProgress() {
+
+}
